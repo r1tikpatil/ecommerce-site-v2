14d94252f5ececd7bd0b6108f2664b6ab749d6a0
r1tikpatil <ritikpatil566@gmail.com>
2023-11-22 16:10:23 +0530

    use redux for categories

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from "react";
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
